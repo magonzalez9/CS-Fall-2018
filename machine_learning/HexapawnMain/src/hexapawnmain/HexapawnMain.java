b7aa983bb6f9993c78c8676670afd72d62815f2a
magonzalez9 <magonzalez@willamette.edu>
2018-09-10 21:32:41 -0700

    Lab 2 initial commit

---
+++ machine_learning/HexapawnMain/src/hexapawnmain/HexapawnMain.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hexapawnmain;
+
+/**
+ *
+ * @author Marco Gonzalez
+ */
+public class HexapawnMain {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        /*Rote learning 
+        Rote Learning - save all of the board positions encountered during play, together with their computed scores 
+        
+        (1) An effective rote-learning technique must include  a procedure to give the program a sense of direction, and it must contain a refined system for cataloging and storing information
+        (2) 
+        
+        
+        
+        
+        */ 
+    }
+    
+}
