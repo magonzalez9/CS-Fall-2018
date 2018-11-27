95f110c31d1bf449fafdbf6da2e9a8b1bdcc0ffa
magonzalez9 <magonzalez@willamette.edu>
2018-11-27 14:58:22 -0800

    Add final lab

---
+++ machine_learning/ml/index.php
@@ -0,0 +1,7 @@
+<?php  
+echo "<h1>Image Detection </h1>";
+
+?>
+
+<img id="image" src="original_photos/1.jpg" onmouseenter="change(this);" onmouseleave="restore();">
+<script type="text/javascript" src="grayscale.js"></script>
