b672f717d2ce78cb08657b208d7c327297b3b169
magonzalez9 <magonzalez@willamette.edu>
2018-12-05 20:24:23 -0800

    updates

--- machine_learning/DragRace/src/dragrace/DragRace.java
+++ machine_learning/DragRace/src/dragrace/DragRace.java
@@ -17,33 +17,8 @@ public class DragRace {
     public static void main(String[] args) {
 //        RaceFrame r = new RaceFrame();
 //        r.setVisible(true);
-
-        int population = 10;
-        int crossover = 1;
-        int muRate = (int) (1 / .001);
-
-        Environment.setState(Environment.Mu.countOnes);
-        Population pList = new Population(population);
-
-        pList.doageneration(muRate, crossover);
-        System.out.println(pList.toString() + "\nFitness avg:" + pList.evaluateFitness());
         
-        for(int i = 0; i < pList.getPopulationList().size(); i++){
-            System.out.println("Speed: " + pList.getPopulationList().get(i).getSpeedStat());
-            System.out.println("Acceleration: " + pList.getPopulationList().get(i).getAccStat());
-            System.out.println("NOS: " + pList.getPopulationList().get(i).getNosStat()); 
-            System.out.println(" ");
-        }
-
-        pList.doageneration(muRate, crossover);
-        System.out.println(pList.toString() + "\nFitness avg:" + pList.evaluateFitness());
-        System.out.println("NEW");
-        for(int i = 0; i < pList.getPopulationList().size(); i++){
-            System.out.println("Speed: " + pList.getPopulationList().get(i).getSpeedStat());
-            System.out.println("Acceleration: " + pList.getPopulationList().get(i).getAccStat());
-            System.out.println("NOS: " + pList.getPopulationList().get(i).getNosStat()); 
-            System.out.println(" ");
-        }
+        Trainer t = new Trainer(10,10,10);
 
 
     }
---
+++ machine_learning/DragRace/src/dragrace/Trainer.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dragrace;
+
+/**
+ *
+ * @author Marco
+ */
+public class Trainer {
+
+    Population pList;
+    RaceAIList rList;
+
+    int population;
+    int crossover;
+    int muRate;
+
+    public Trainer(int population, int crossover, double muRate) {
+        pList = new Population(population);
+        rList = new RaceAIList();
+
+        this.population = population;
+        this.crossover = crossover;
+        this.muRate = (int) (1 / muRate);
+    }
+
+    public void train() {
+        pList.doageneration(muRate, crossover);
+        System.out.println(pList.toString() + "\nFitness avg:" + pList.evaluateFitness());
+
+        for (int i = 0; i < pList.getPopulationList().size(); i++) {
+            System.out.println("Speed: " + pList.getPopulationList().get(i).getSpeedStat());
+            System.out.println("Acceleration: " + pList.getPopulationList().get(i).getAccStat());
+            System.out.println("NOS: " + pList.getPopulationList().get(i).getNosStat());
+            System.out.println(" ");
+        }
+
+        pList.doageneration(muRate, crossover);
+        System.out.println(pList.toString() + "\nFitness avg:" + pList.evaluateFitness());
+        System.out.println("NEW");
+        for (int i = 0; i < pList.getPopulationList().size(); i++) {
+            System.out.println("Speed: " + pList.getPopulationList().get(i).getSpeedStat());
+            System.out.println("Acceleration: " + pList.getPopulationList().get(i).getAccStat());
+            System.out.println("NOS: " + pList.getPopulationList().get(i).getNosStat());
+            System.out.println(" ");
+        }
+    }
+
+}
