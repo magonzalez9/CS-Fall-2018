0aaff4436942ba7ff21c79a68051d53f706afbb4
marcoAGonzalez <gonzalezmarco0131@gmail.com>
2019-01-20 22:15:04 -0800

    Updates to resume and thesis

---
+++ ml/index.php
@@ -0,0 +1,48 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Face Detection Application</title>
+	<!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"/> -->
+	<script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+	<script src="face_detection/dist/jquery.facedetection.min.js"> </script>
+	<script src="photo_approval.js"></script>
+	<style>
+		.face-box {
+  		border: 1px solid #f00;
+  		position:absolute;
+		}
+	</style>
+</head>
+<body>
+<h1>Photo Detection Application</h1>
+<div id="wrapper"></div>
+<img id="picture" src="original_photos/color/1.jpg">
+<br />
+<button onclick="getNextPhoto(false)">Prev</button>
+<button onclick="getNextPhoto(true)">Next</button>
+<br />
+<br />
+<fieldset>
+<!--Image setings-->
+Interval: <input type="number" id="interval" name="interval" min="1" max="100" value="4"> <br/> <br/>
+minNeighbors: <input type="number" id="minNeighbors" name="minNeighbors" min="1" max="100" value="1"><br /><br />
+Confidence: <input type="number" id="confidence" name="confidence" min="1" max="100" value=""><br/><br/>
+
+<br />
+<label>Grayscale</label><br />
+<input type="radio" name="grayscale" id="grayscale" value="true"> True<br>
+<input type="radio" name="grayscale" id="grayscale" value="false" checked="checked"> False<br>
+<br>
+<button onclick="analyzeImage()">Analyze</button>
+<p id="pixels"> </p>
+
+
+
+<p id="response"></p>
+</fieldset>
+
+</body>
+</html>
+
+
---
+++ ml/photo_approval.js
@@ -0,0 +1,134 @@
+var img = document.getElementById("picture");
+var img = 1; 
+// 60984
+
+function getImagePixels()  {
+	// Create canvas and get image
+    var c = document.createElement("canvas");
+	var ctx = c.getContext("2d");
+   
+    // Set canvas dimensions proportional to image dimensions
+    c.width = img.width; 
+    c.height = img.height; 
+
+    // Draw image onto the canvas and get image data
+    ctx.drawImage(img, 0, 0);
+    var imgData = ctx.getImageData(0, 0, c.width, c.height);
+
+
+    // Loop through every pixel in order to determine its color
+    var pixelArray = new Array(); 
+    var j = 0; 
+
+    // Multi dimensional array (x by x)
+
+	for (var i = 0; i < imgData.data.length; i+=4) {
+		pixelArray[j] = getGrayScaleValue(imgData.data[i], imgData.data[i+1], imgData.data[i+2]);
+		j++; 
+  	}
+
+    return pixelArray; 
+} // --end of function getImagePixels
+
+function getGrayScaleValue(r, g, b){
+	// Apply affine transfomation to rgb values in order to color-categorize the pixels from 0-1
+	// E.g. 0 (lighter pixel) - 1 (darker pixel) 
+	// f(t)=(d−c)(b−a) / (t−a) + c
+	var rValue = parseFloat(((r - 255) * (1 - 0) / (0 - 255) + 0).toFixed(2));
+	var gValue = parseFloat(((g - 255) * (1 - 0) / (0 - 255) + 0).toFixed(2));
+	var bValue = parseFloat(((b - 255) * (1 - 0) / (0 - 255) + 0).toFixed(2));
+
+	var sumAvg = ((rValue + gValue + bValue) / 3).toFixed(2);
+
+	// The lower the number the greater the darkness definition 
+	var bwSensitivity = .5; 
+
+	if (sumAvg >= bwSensitivity) {
+		return "*";
+	} else if (sumAvg < bwSensitivity){ 
+		return "_"; 
+	}
+} // --end of function getGrayScaleValue
+
+function debug(){
+	pixelArray = getImagePixels(); 
+
+	var cleanStr = ""; 
+	var multiplier = img.width; 
+	for (var i = 0; i < pixelArray.length; i++) {
+		cleanStr += pixelArray[i] + "";
+
+		if (i == multiplier) {
+			cleanStr += "|<br />"; 
+			multiplier+=img.width; 
+		}
+	}
+
+	document.getElementById("pixels").innerHTML = cleanStr;
+}
+function analyzeImage(){
+	// Get settings 
+	var interval = parseInt(document.getElementById('interval').value, 10); 
+	var minNeighbors = parseInt(document.getElementById('minNeighbors').value, 10);
+	var confidence = parseInt(document.getElementById('confidence').value, 10); 
+	var grayscale = document.querySelector('input[name="grayscale"]:checked').value;
+
+	if (grayscale == "false") {
+		grayscale = false; 
+	} else {
+		grayscale = true; 
+	}
+
+	//alert( interval + "\n" +  minNeighbors + "\n" +  grayscale);
+
+	/* RETURN VALUES */
+	// x — X coord of the face in the picture
+	// y — Y coord of the face in the picture
+	// width — Width of the face
+	// height — Height of the face
+	// positionX — X position relative to the document
+	// positionY — Y position relative to the document
+	// offsetX — X position relative to the offset parent
+	// offsetY — Y position relative to the offset parent
+	// scaleX — Ratio between original image width and displayed width
+	// scaleY — Ratio between original image height and displayed height
+	// confidence — Level of confidence position relative to the document
+
+	// Get the face location
+    $('#picture').faceDetection({
+    	interval: interval,
+    	minNeighbors: minNeighbors, 
+    	confidence: null, 
+    	async: false, 
+    	grayscale: grayscale,
+        complete: function (faces) {
+        	// Get the count
+        	console.log(faces.length); 
+        	if (faces.length > 0 ) {
+        		console.log(faces);
+			    $div = $("<div>", {"class": "face-box"});
+			    $div.css('top', faces[0].positionY);
+			    $div.css('left', faces[0].positionX );
+			    $div.css('width', faces[0].width );
+			    $div.css('height', faces[0].height );
+			    $("#wrapper").append($div);
+			    document.getElementById("response").innerHTML = "Confidence: " + faces[0].confidence;
+        	} else {
+        		console.log("NO FACES FOUND!"); 
+        	}
+     
+        }
+
+    });
+}
+
+function getNextPhoto(next){
+	if (next == true) {
+		img++;
+	} else {
+
+		img--; 
+	}
+	document.getElementById("wrapper").innerHTML = "";
+	document.getElementById("picture").setAttribute("src", "original_photos/color/" + img + ".jpg");
+}
