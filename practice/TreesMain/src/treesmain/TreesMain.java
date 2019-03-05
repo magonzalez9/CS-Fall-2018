c3df5decdf4ae25eed0dbe13e419c5e4e16f0808
marcoAGonzalez <gonzalezmarco0131@gmail.com>
2019-03-05 14:36:37 -0800

    Algorithms practice

--- practice/TreesMain/src/treesmain/TreesMain.java
+++ practice/TreesMain/src/treesmain/TreesMain.java
@@ -16,19 +16,20 @@ public class TreesMain {
     public static void main(String[] args) {
         
         BinaryTree myTree = new BinaryTree(); 
-        myTree.addNode(10);
-        myTree.addNode(15);
-        myTree.addNode(5);
+        myTree.addNode(18);
+        
+        myTree.addNode(21);
+        myTree.addNode(22);
+        myTree.addNode(20);
         myTree.addNode(6);
         myTree.addNode(4); 
         myTree.addNode(3);
 
-        
         myTree.printInOrder(myTree.root);
         System.out.println("");
-        myTree.printPreOrder(myTree.root);
+        myTree.printPreOrder(myTree.root.rightChild);
         System.out.println("");
-        myTree.printPostOrder(myTree.root);
+        myTree.printPostOrder(myTree.root.rightChild.rightChild);
     }
     
 }
