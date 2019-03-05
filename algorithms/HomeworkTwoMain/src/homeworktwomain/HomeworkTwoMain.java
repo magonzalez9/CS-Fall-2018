9f71632c2f6e0b7e8f6621b5134f465086e3e3c3
marcoAGonzalez <gonzalezmarco0131@gmail.com>
2019-03-04 21:40:18 -0800

    Finish Algorithms HW 2

--- algorithms/HomeworkTwoMain/src/homeworktwomain/HomeworkTwoMain.java
+++ algorithms/HomeworkTwoMain/src/homeworktwomain/HomeworkTwoMain.java
@@ -18,35 +18,36 @@ public class HomeworkTwoMain {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        testQuickSort(100);
-
+        testSorts(100);
+        System.out.println("");
+        testSorts(1000);
+        System.out.println("");
+        testSorts(10000);
+        System.out.println("");
+        testSorts(100000);
+        
     }
 
-    public static void testHeapSort(int nSize) {
+    public static void testSorts(int nSize) {
         int[] A = randomArray(nSize);
-        final long start = System.nanoTime();
+        int[] B = Arrays.copyOf(A, nSize);
 
-        final long end = System.nanoTime();
-        System.out.println("Took: " + ((end - start)) + " nano seconds");
-
-    }
-
-    public static void testQuickSort(int nSize) {
-        int[] A = randomArray(nSize);
-        int[] output;
-        final long start = System.nanoTime(); // Start timer
+        final long startQ = System.nanoTime(); // Start timer
         // Perform quicksort
-        output = quickSort(A, 0, nSize - 1);
-        final long end = System.nanoTime(); // End timer
+        quickSort(A, 0, nSize - 1);
+        final long endQ = System.nanoTime(); // End timer
 
-        // Print output
-        System.out.println(Arrays.toString(output));
-        System.out.println("Took: " + ((end - start)) + " nano seconds");
+        System.out.println("Quick Sort Took: " + ((endQ - startQ)/Math.pow(10, 9)) + " nano seconds for size: " + nSize + " ("+ (endQ - startQ)+" nano secs)");
 
+        final long startH = System.nanoTime(); // Start timer
+        // Perform quicksort
+        heapSort(B);
+        final long endH = System.nanoTime(); // End timer
+        System.out.println("Heap Sort Took: " + ((endH - startH)/ Math.pow(10,9)) + " nano seconds for size: " + nSize+ " ("+ (endH - startH)+" nano secs)");
     }
+// -------------Start quick sort ---------------------------------------------
 
     public static int[] quickSort(int[] A, int low, int high) {
-
         if (low < high) {
             int pivot = partition(A, low, high);
 
@@ -77,10 +78,57 @@ public class HomeworkTwoMain {
         return (i + 1);
     }
 
-    public static void heapSort() {
-        
+// ----------------END quick sort --------------------------------------------
+// ----------------Start of HeapSort -----------------------------------------
+    public static int[] heapSort(int[] A) {
+        A = buildMaxHeap(A);
+
+        for (int i = A.length - 1; i >= 0; i--) {
+            int tmp = A[0];
+            A[0] = A[i];
+            A[i] = tmp;
+
+            heapify(A, i, 0);
+        }
+
+        return A;
     }
 
+    public static int[] buildMaxHeap(int[] A) {
+        int n = A.length;
+
+        for (int i = n / 2 - 1; i >= 0; i--) {
+            heapify(A, n, i);
+        }
+        return A;
+    }
+
+    public static void heapify(int[] A, int n, int i) {
+        int max = i;
+        int left = (2 * i) + 1;
+        int right = (2 * i) + 2;
+
+        // left child > root
+        if (left < n && (A[left] > A[max])) {
+            max = left;
+        }
+        // right child > max
+        if (right < n && A[right] > A[max]) {
+            max = right;
+        }
+        // max is not root
+        if (max != i) {
+            int tmp = A[i];
+            A[i] = A[max];
+            A[max] = tmp;
+
+            // heapify affected sub tree
+            heapify(A, n, max);
+        }
+
+    }
+
+    // ----------------End of HeapSort -----------------------------------------
     public static int[] randomArray(int size) {
 
         int[] A = new int[size];
