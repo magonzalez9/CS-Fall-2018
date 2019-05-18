5d03f45e870ecf01215f13ec2007f55b4d553deb
magonzalez9 <magonzalez@willamette.edu>
2019-05-18 12:55:52 -0700

    CMS with React

---
+++ cms/test/index.php
@@ -0,0 +1,17 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+
+<h1>Edit Content</h1>
+<p>This is a sample page</p>
+
+<div>
+	<?php include('../index.html'); ?>
+</div>
+
+</body>
+</html>
